771cc2975cedf32399db1ed7425c7a51300e2864
Igor Dev <igormonteiro.dev@gmail.com>
2022-08-15 00:13:51 +0200

    done

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,41 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
-  shuffleCards() {
-    // ... write your code here
+  shuffleCards(arr) {
+    if (!arr) return undefined;
+
+    let newArr = [];
+
+    let i = arr.length,
+      a,
+      temp; // a is to generate random index and temp is to swap the values
+    while (--i > 0) {
+      a = Math.floor(Math.random() * i);
+      temp = arr[a];
+      arr[a] = arr[i];
+      arr[i] = temp;
+    }
+    return newArr;
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked++;
+
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    } else {
+      return false;
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed < 12) return false;
+    if (this.pairsGuessed === 12) return true;
   }
 }
